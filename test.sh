fa9ee07701c79550514295fdcc4a0f088d5b0338
Antoine Delorle <antoine.delorme@gmail.com>
2019-12-01 18:32:40 +0100

    remove all

--- test.sh
+++
@@ -1,1 +0,0 @@
-gcc -fsanitize=address -Wall -Wextra -Werror -I./libft/ -L./libft/ -lft get_next_line.c test.c && ./a.out $1
--- test_val.sh
+++
@@ -1,1 +0,0 @@
-gcc -g -I./libft/ -L./libft/ -lft get_next_line.c test.c && ./a.out $1
